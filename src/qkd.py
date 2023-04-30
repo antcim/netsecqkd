74d9eed3b6cbe205e7226e207fb5b8505f7588d4
Antonio Cimino <98947590+antcim@users.noreply.github.com>
2023-04-30 11:15:31 +0200

    src refactoring

--- src/qkd.py
+++ src/qkd.py
@@ -8,6 +8,10 @@ from sequence.components.optical_channel import QuantumChannel, ClassicalChannel
 from sequence.qkd.BB84 import pair_bb84_protocols
 from sequence.topology.qkd_topo import QKDTopo
 
+import networkx as nx
+import json
+
+
 class KeyManager():
     def __init__(self, timeline, keysize, num_keys):
         self.timeline = timeline
@@ -16,30 +20,44 @@ class KeyManager():
         self.num_keys = num_keys
         self.keys = []
         self.times = []
-        
+
     def send_request(self):
         for p in self.lower_protocols:
-            p.push(self.keysize, self.num_keys) # interface for BB84 to generate key
-            
-    def pop(self, info): # interface for BB84 to return generated keys
+            # interface for BB84 to generate key
+            p.push(self.keysize, self.num_keys)
+
+    def pop(self, info):  # interface for BB84 to return generated keys
         self.keys.append(info)
         self.times.append(self.timeline.now() * 1e-9)
 
 
-def test(sim_time, keysize):
-    """
-    sim_time: duration of simulation time (ms)
-    keysize: size of generated secure key (bits)
-    """
-    NUM_KEYS = 25
+def genNetwork(filename):
+    # generate random graph
+    G = nx.random_lobster(10, 0.2, 0.25)
+    # JSON representation
+    jsonG = nx.node_link_data(G)
+    with open(filename, 'w') as f:
+        json.dump(jsonG, f, ensure_ascii=False)
 
+
+def readConfig(filepath):
     network_config = "src/testnet.json"
-    network_topo = QKDTopo(network_config)
-    print(network_topo.get_qchannels())
+    return QKDTopo(network_config)
+
+
+def test(keysize):
+    NUM_KEYS = 25
+    PRINT_KEYS = False
+
+    genNetwork('src/rndNetConf.json')
+    network_topo = readConfig("src/testnet.json")
     tl = network_topo.get_timeline()
-    
+
+    for i, node in enumerate(network_topo.get_nodes_by_type(QKDTopo.QKD_NODE)):
+        for key in node.qchannels.keys():
+            print(node.qchannels[key].name)
+
     n1 = network_topo.get_nodes_by_type(QKDTopo.QKD_NODE)[0]
-    # print(n1.qchannels())
     n2 = network_topo.get_nodes_by_type(QKDTopo.QKD_NODE)[1]
     n1.set_seed(0)
     n2.set_seed(1)
@@ -51,7 +69,7 @@ def test(sim_time, keysize):
     qc1 = network_topo.get_qchannels()[1]
     qc0.polarization_fidelity = 0.97
     qc1.polarization_fidelity = 0.97
-    
+
     # instantiate our written keysize protocol
     km1 = KeyManager(tl, keysize, NUM_KEYS)
     km1.lower_protocols.append(n1.protocol_stack[0])
@@ -59,30 +77,32 @@ def test(sim_time, keysize):
     km2 = KeyManager(tl, keysize, NUM_KEYS)
     km2.lower_protocols.append(n2.protocol_stack[0])
     n2.protocol_stack[0].upper_protocols.append(km2)
-    
+
     # start simulation and record timing
     tl.init()
     km1.send_request()
     tick = time.time()
     tl.run()
     print("execution time %.2f sec" % (time.time() - tick))
-    
+
     # display our collected metrics
     plt.plot(km1.times, range(1, len(km1.keys) + 1), marker="o")
     plt.xlabel("Simulation time (ms)")
     plt.ylabel("Number of Completed Keys")
     plt.show()
-    
+
     print("key error rates:")
     for i, e in enumerate(n1.protocol_stack[0].error_rates):
         print("\tkey {}:\t{}%".format(i + 1, e * 100))
 
-    print("Node 1 keys:")
-    for i, key in enumerate(km1.keys):
-        print("\t{0:0128b}".format(key))
-    
-    print("Node 2 keys:")
-    for i, key in enumerate(km2.keys):
-        print("\t{0:0128b}".format(key))
+    if PRINT_KEYS:
+        print("Node 1 keys:")
+        for i, key in enumerate(km1.keys):
+            print("\t{0:0128b}".format(key))
 
-test(5000, 128)
+        print("Node 2 keys:")
+        for i, key in enumerate(km2.keys):
+            print("\t{0:0128b}".format(key))
+
+
+test(128)
