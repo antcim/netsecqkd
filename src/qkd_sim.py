532f7dcbf87c493e862bd7d4761e035c4d4ceed8
antcim <98947590+antcim@users.noreply.github.com>
2023-05-05 17:37:23 +0200

    2 way qkd trial - not working

--- src/qkd_sim.py
+++ src/qkd_sim.py
@@ -65,7 +65,7 @@ def runQKD(network, keysize):
     for i, node in enumerate(network.get_nodes_by_type(QKDTopo.QKD_NODE)):
         for key in node.cchannels.keys():
             sender = node
-            receiver = copy.copy(node)
+            receiver = copy.deepcopy(node)
             sim_nodes[node.name].addSRQKDNode(SRQKDNode(sender, receiver))
             print("Channel: " + str(node.cchannels[key].name))
             print("Sender: " + str(node.cchannels[key].sender.name))
@@ -77,38 +77,72 @@ def runQKD(network, keysize):
             print(key, 'sender : ', srqknode.sender.name)
             print(key, 'receiver : ', srqknode.receiver.name)
 
+    # QKD from 0 to 1
+    n0 = sim_nodes["node0"].srqkdnodes[0].sender
+    n1 = sim_nodes["node1"].srqkdnodes[0].receiver
 
-    return
-
-    n1 = network.get_nodes_by_type(QKDTopo.QKD_NODE)[0]
-    n2 = network.get_nodes_by_type(QKDTopo.QKD_NODE)[1]
-
+    print(n0.name)
     print(n1.name)
-    print(n2.name)
     print(tl.stop_time)
 
-    n1.set_seed(0)
-    n2.set_seed(1)
-    pair_bb84_protocols(n1.protocol_stack[0], n2.protocol_stack[0])
+    n0.set_seed(0)
+    n1.set_seed(1)
+    pair_bb84_protocols(n0.protocol_stack[0], n1.protocol_stack[0])
 
-    cc0 = network.get_cchannels()[0]
-    cc1 = network.get_cchannels()[1]
-    qc0 = network.get_qchannels()[0]
-    qc1 = network.get_qchannels()[1]
+    for key, channel in n0.cchannels.items():
+        print(key + " "+ channel.name)
+
+    # index of dict is the endpoint of the channel
+    cc0 = n0.cchannels["node1"]
+    cc1 = n1.cchannels["node0"]
+    qc0 = n0.qchannels["node1"]
+    qc1 = n1.qchannels["node0"]
     qc0.polarization_fidelity = 0.97
     qc1.polarization_fidelity = 0.97
 
     # instantiate our written keysize protocol
     km1 = KeyManager(tl, keysize, NUM_KEYS)
-    km1.lower_protocols.append(n1.protocol_stack[0])
-    n1.protocol_stack[0].upper_protocols.append(km1)
+    km1.lower_protocols.append(n0.protocol_stack[0])
+    n0.protocol_stack[0].upper_protocols.append(km1)
     km2 = KeyManager(tl, keysize, NUM_KEYS)
-    km2.lower_protocols.append(n2.protocol_stack[0])
-    n2.protocol_stack[0].upper_protocols.append(km2)
+    km2.lower_protocols.append(n1.protocol_stack[0])
+    n1.protocol_stack[0].upper_protocols.append(km2)
+
+    # QKD from 1 to 0
+    n2 = sim_nodes["node1"].srqkdnodes[0].sender
+    n3 = sim_nodes["node0"].srqkdnodes[0].receiver
+
+    print(n2.name)
+    print(n3.name)
+    print(tl.stop_time)
+
+    n2.set_seed(0)
+    n3.set_seed(1)
+    pair_bb84_protocols(n2.protocol_stack[0], n3.protocol_stack[0])
+
+    for key, channel in n2.cchannels.items():
+        print(key + " "+ channel.name)
+
+    # index of dict is the endpoint of the channel
+    cc0 = n2.cchannels["node0"]
+    cc1 = n3.cchannels["node1"]
+    qc0 = n2.qchannels["node0"]
+    qc1 = n3.qchannels["node1"]
+    qc0.polarization_fidelity = 0.97
+    qc1.polarization_fidelity = 0.97
+
+    # instantiate our written keysize protocol
+    km3 = KeyManager(tl, keysize, NUM_KEYS)
+    km3.lower_protocols.append(n2.protocol_stack[0])
+    n2.protocol_stack[0].upper_protocols.append(km3)
+    km4 = KeyManager(tl, keysize, NUM_KEYS)
+    km4.lower_protocols.append(n3.protocol_stack[0])
+    n3.protocol_stack[0].upper_protocols.append(km4)
 
     # start simulation and record timing
     tl.init()
     km1.send_request()
+    km3.send_request()
     tick = time.time()
     tl.run()
     print("execution time %.2f sec" % (time.time() - tick))
@@ -119,8 +153,18 @@ def runQKD(network, keysize):
     plt.ylabel("Number of Completed Keys")
     plt.show()
 
+    # display our collected metrics
+    plt.plot(km3.times, range(1, len(km3.keys) + 1), marker="o")
+    plt.xlabel("Simulation time (ms)")
+    plt.ylabel("Number of Completed Keys")
+    plt.show()
+
     print("key error rates:")
-    for i, e in enumerate(n1.protocol_stack[0].error_rates):
+    for i, e in enumerate(n0.protocol_stack[0].error_rates):
+        print("\tkey {}:\t{}%".format(i + 1, e * 100))
+
+    print("key error rates:")
+    for i, e in enumerate(n3.protocol_stack[0].error_rates):
         print("\tkey {}:\t{}%".format(i + 1, e * 100))
 
     if PRINT_KEYS:
