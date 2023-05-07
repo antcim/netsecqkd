a08c80d23b9bc9e2dd600a191e6959e917fbe038
antcim <98947590+antcim@users.noreply.github.com>
2023-05-07 23:28:54 +0200

    draw graph to file

--- src/qkd_sim.py
+++ src/qkd_sim.py
@@ -67,11 +67,11 @@ def genNetwork(filepath):
 
     if draw:
         pos = nx.spring_layout(G)
-        nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_size = 500)
+        nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_size = 200)
         nx.draw_networkx_labels(G, pos)
         nx.draw_networkx_edges(G, pos, edge_color='r', arrows=True)
         nx.draw_networkx_edges(G, pos, arrows=False)
-        plt.show()
+        plt.savefig("src/graph.png", dpi=500, orientation = 'landscape', bbox_inches='tight')
 
 
 def readConfig(filepath):
